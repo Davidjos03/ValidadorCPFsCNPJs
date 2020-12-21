781043479b02820fe6081fffd269a11426ab50a3
Davidjos03 <38574135+Davidjos03@users.noreply.github.com>
2020-12-21 02:41:24 -0200

    .

--- validador/src/main/java/com/validador/SwaggerConfig.java
+++ validador/src/main/java/com/validador/SwaggerConfig.java
@@ -1,21 +1,18 @@
 package com.validador;
 
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import org.springframework.context.annotation.Bean;
-
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-
-
-import static springfox.documentation.builders.PathSelectors.regex;
-
-import java.util.ArrayList;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
@@ -26,7 +23,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.validador"))
-                .paths(regex("/validador.*"))
+                .paths(PathSelectors.any())
                 .build()
                 .apiInfo(metaInfo());
     }
--- validador/src/main/java/com/validador/resources/TransacaoResource.java
+++ validador/src/main/java/com/validador/resources/TransacaoResource.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 import javax.validation.Valid;
 
-import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
